616e417f89462149a25fb7e6a0e42ecd3936f846
gerhard <gerhardkleintjie@gmail.com>
2021-02-26 21:14:54 +0200

    DataAccessLayer to text file

--- DataAccessLayer/DB.java
+++ DataAccessLayer/DB.java
@@ -1,7 +1,34 @@
 package DataAccessLayer;
 import java.io.*;
+import java.io.FileWriter;
+
 
 public class DB 
 {
+
+    
+
+    public static void main(String[] args) throws IOException {
+
+        Booking bk = new Booking();  //instansiated class named Booking
+        Event ev = new Event();     //instansiated class named Event
+        
+        FileWriter fw = new FileWriter("text.txt");   //FileWriter opening connection to the text file
+
+        for(int i = 0; i < bk.Booking.size(); i++){ //for loop iterating through arrayList, writing each entry to the text file
+            fw.write(bk.Booking.get(i));
+        }
+
+        for(int i = 0; i < ev.Event.size(); i++){ //for loop iterating through arrayList, writing each entry to the text file
+            fw.write(ev.Event.get(i));
+        }
+
+        fw.close(); //closing the connection to the text file. 
+
+   } 
+    
+
+
+
      
 }
