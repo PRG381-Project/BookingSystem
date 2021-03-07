d9741c5f63c4d14c610c4a6daee1b7947aba9873
gerhard <gerhardkleintjie@gmail.com>
2021-03-07 12:21:30 +0200

    Modified code

--- PresentationLayer/UserInput.java
+++ PresentationLayer/UserInput.java
@@ -27,16 +27,40 @@ public class UserInput {
         System.out.println("Enter the amount of people attending the event:");
         amountOfPeople = Integer.parseInt(scan.nextLine());
 
-        System.out.println("Enter the meal the adults will have:");
+        System.out.println("Enter the meal the adults will have from the options bellow:");
+        System.out.println("Burgers");
+        System.out.println("Pasta");
+        System.out.println("Pizza");
+        System.out.println("Salad");
+        System.out.println("Fish & Chips");
+        System.out.println("Steak & Wedges");
         adultMeal = scan.nextLine();
 
-        System.out.println("Enter the meal the children will have:");
+        System.out.println("Enter the meal the children will have from the options bellow:");
+        System.out.println("Burgers");
+        System.out.println("Fish fingers");
+        System.out.println("Pizza");
+        System.out.println("Chips");
+        System.out.println("Fish & Chips");
+        System.out.println("Mac & Cheese");
         kidsMeal = scan.nextLine();
 
-        System.out.println("Enter the drink of choice:");
+        System.out.println("Enter the drink of choice from the options bellow:");
+        System.out.println("Coke");
+        System.out.println("Cream Soda");
+        System.out.println("Fanta");
+        System.out.println("Wine");
+        System.out.println("Whiskey");
+        System.out.println("Beer");
+        System.out.println("Orange juice");
+        System.out.println("Apple juice");
         drink = scan.nextLine();
 
-        System.out.println("Enter dessert of choice");
+        System.out.println("Enter dessert of choice from the options bellow:");
+        System.out.println("Ice cream");
+        System.out.println("Chocolate pudding");
+        System.out.println("Cheese cake");
+        System.out.println("Chocolate Moose");
         dessert = scan.nextLine();
 
         Booking b = new Booking(amountOfPeople, adultMeal, kidsMeal, drink, dessert);
@@ -46,12 +70,12 @@ public class UserInput {
     public Event getEvent() throws ParseException
     {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter event type:");
+        System.out.println("Enter event type (Wedding/Birthday/Funeral/Business Event):");
         eventType = scan.nextLine();
         System.out.println("Enter event date, Format: dd/MM/yyyy");
         eventDate = scan.nextLine();
         Date date1 = new SimpleDateFormat("dd/MM/yyyy").parse(eventDate);
-        System.out.println("Enter event address");
+        System.out.println("Enter event address:");
         eventAddress = scan.nextLine();
 
         Event ev = new Event(eventType, date1, eventAddress);
